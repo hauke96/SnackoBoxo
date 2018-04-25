b492f475263c11b0eb44f7b827b497024df7fda3
Hauke Stieler <mail@hauke-stieler.de>
2018-04-25 20:06:23 +0200

    Add first click listener

--- app/src/main/java/de/hauke_stieler/snackoboxo/MainActivity.java
+++ app/src/main/java/de/hauke_stieler/snackoboxo/MainActivity.java
@@ -2,6 +2,8 @@ package de.hauke_stieler.snackoboxo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.Pay10CentButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pay(10);
+            }
+        });
+    }
+
+    private void pay(int cent){
+
     }
 }
