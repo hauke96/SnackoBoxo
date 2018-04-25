fb53d9151d5b20ddaeea79a9244a559f19585a98
Hauke Stieler <mail@hauke-stieler.de>
2018-04-25 20:14:13 +0200

    Add all button listeners

--- app/src/main/java/de/hauke_stieler/snackoboxo/MainActivity.java
+++ app/src/main/java/de/hauke_stieler/snackoboxo/MainActivity.java
@@ -12,15 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.Pay10CentButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                pay(10);
-            }
-        });
+        this.<Button>findViewById(R.id.Pay10CentButton).setOnClickListener(v -> pay(10));
+        this.<Button>findViewById(R.id.Pay20CentButton).setOnClickListener(v -> pay(20));
+        this.<Button>findViewById(R.id.Pay50CentButton).setOnClickListener(v -> pay(50));
+        this.<Button>findViewById(R.id.Pay1EuroButton).setOnClickListener(v -> pay(100));
+        this.<Button>findViewById(R.id.Pay2EuroButton).setOnClickListener(v -> pay(200));
+
+        this.<Button>findViewById(R.id.BuyFor10CentButton).setOnClickListener(v -> buy(10));
+        this.<Button>findViewById(R.id.BuyFor20CentButton).setOnClickListener(v -> buy(20));
+        this.<Button>findViewById(R.id.BuyFor50CentButton).setOnClickListener(v -> buy(50));
+        this.<Button>findViewById(R.id.BuyFor1EuroButton).setOnClickListener(v -> buy(100));
+        this.<Button>findViewById(R.id.BuyFor2EuroButton).setOnClickListener(v -> buy(200));
     }
 
     private void pay(int cent){
+
+    }
+
+    private void buy(int cent){
 
     }
 }
