fdf5c70b896f2144ea8a83662c5abdb5a205fea8
Hauke Stieler <mail@hauke-stieler.de>
2018-05-30 01:39:53 +0200

    Generate buttons programmatically from list of integers

--- app/src/main/java/de/hauke_stieler/snackoboxo/MainActivity.java
+++ app/src/main/java/de/hauke_stieler/snackoboxo/MainActivity.java
@@ -3,17 +3,27 @@ package de.hauke_stieler.snackoboxo;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hauke on 25.04.18.
- *
+ * <p>
  * The app has only this activity, that's why it's called "MainActivity" ;)
  */
 public class MainActivity extends AppCompatActivity {
 
     IPaymentService _paymentService;
+
+    List<Integer> _moneyValues;
+    private LinearLayout.LayoutParams _buttonLayoutParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,26 +32,58 @@ public class MainActivity extends AppCompatActivity {
         _paymentService = new PaymentServiceImpl(sharedPreference);
         _paymentService.DepositChangedEvent.add(value -> updateView(value));
 
+        //TODO read these from shared preferences. Create a service to access these (which handles default values etc.)
+        _moneyValues = new ArrayList<>();
+        _moneyValues.add(10);
+        _moneyValues.add(20);
+        _moneyValues.add(40);
+        _moneyValues.add(50);
+        _moneyValues.add(80);
+        _moneyValues.add(100);
+        _moneyValues.add(200);
+
+        _buttonLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        _buttonLayoutParams.setMargins(0, toPx(5), 0, toPx(5));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        for (int value : _moneyValues) {
+            addDepositButton(value);
+            addBuyForButton(value);
+        }
+
         updateView(_paymentService.getDeposit());
+    }
 
-        this.<Button>findViewById(R.id.Deposit10CentButton).setOnClickListener(v -> deposit(10));
-        this.<Button>findViewById(R.id.Deposit20CentButton).setOnClickListener(v -> deposit(20));
-        this.<Button>findViewById(R.id.Deposit40CentButton).setOnClickListener(v -> deposit(40));
-        this.<Button>findViewById(R.id.Deposit50CentButton).setOnClickListener(v -> deposit(50));
-        this.<Button>findViewById(R.id.Deposit80CentButton).setOnClickListener(v -> deposit(80));
-        this.<Button>findViewById(R.id.Deposit1EuroButton).setOnClickListener(v -> deposit(100));
-        this.<Button>findViewById(R.id.Deposit2EuroButton).setOnClickListener(v -> deposit(200));
+    private void addDepositButton(int cents) {
+        Button button = new Button(this);
+        button.setLayoutParams(_buttonLayoutParams);
+        button.setText("+ " + toCurrencyString(cents));
+        button.setOnClickListener(v -> deposit(cents));
 
-        this.<Button>findViewById(R.id.BuyFor10CentButton).setOnClickListener(v -> buy(10));
-        this.<Button>findViewById(R.id.BuyFor20CentButton).setOnClickListener(v -> buy(20));
-        this.<Button>findViewById(R.id.BuyFor40CentButton).setOnClickListener(v -> buy(40));
-        this.<Button>findViewById(R.id.BuyFor50CentButton).setOnClickListener(v -> buy(50));
-        this.<Button>findViewById(R.id.BuyFor80CentButton).setOnClickListener(v -> buy(80));
-        this.<Button>findViewById(R.id.BuyFor1EuroButton).setOnClickListener(v -> buy(100));
-        this.<Button>findViewById(R.id.BuyFor2EuroButton).setOnClickListener(v -> buy(200));
+        this.<LinearLayout>findViewById(R.id.DepositButtonLayout).addView(button);
+    }
+
+    private void addBuyForButton(int cents) {
+        Button button = new Button(this);
+        button.setLayoutParams(_buttonLayoutParams);
+        button.setText("- " + toCurrencyString(cents));
+        button.setOnClickListener(v -> buy(cents));
+
+        this.<LinearLayout>findViewById(R.id.BuyForButtonLayout).addView(button);
+    }
+
+    private String toCurrencyString(int cents) {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        return formatter.format(cents / 100.0);
+    }
+
+    private int toPx(int dpValue) {
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_PX,
+                dpValue,
+                getResources().getDisplayMetrics());
     }
 
     private void deposit(int cent) {
